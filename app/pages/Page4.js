b1ad0ad0823dde685649ddfd56fc2c3694a497c4
kennedynguyen1 <kennedynguyen@ucsb.edu>
2025-03-06 14:27:01 -0800

    pages 2-5 done w imported components

--- app/pages/Page4.js
+++ app/pages/Page4.js
@@ -1,6 +1,71 @@
 import React from 'react';
 import BasePage from './BasePage';
+import Shark from '../../components/Shark'
+import { View, StyleSheet } from 'react-native';
+import Wave from '../../components/Wave';
+import SpeechBubble from '../../components/SpeechBubble';
+import ReviewProcess from '../../components/ReviewProcess';
 
 export default function Page4() {
-  return <BasePage pageNumber={4} />;
+  return (
+    <View style={styles.container}>
+      <BasePage pageNumber={4} />
+      <View style={styles.waveContainer}>
+        <Wave />
+      </View>
+      <View style={styles.sharkContainer}>
+        <Shark />
+      </View>
+      <View style={styles.speechBubbleContainer}>
+        <SpeechBubble />
+      </View>
+      <View style={styles.reviewProcessContainer}>
+        <ReviewProcess />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+  reviewProcessContainer: {
+    position: 'absolute',
+    bottom: -40,     // This will align to bottom
+    left: 85,
+    zIndex: 1, 
+    transform: [
+      { scale: 0.80}
+    ]
+  },
+  waveContainer: {
+    position: 'absolute',
+    bottom: -80,     // This will align to bottom
+    left: 0,
+    right: 0,
+    zIndex: 1,  // Waves will be above background
+  },
+  sharkContainer: {
+    position: 'absolute',
+    top: 275,    // pixels from top
+    left: -75,   // pixels from left
+    zIndex: 2,
+    transform: [
+      { scale: 0.75 },
+      { rotate: '-5deg' }
+    ]
+  },
+  speechBubbleContainer: {
+    position: 'absolute',
+    bottom: 125,     // This will align to bottom
+    left: -275,
+    right: 0,
+    zIndex: 2,  // Waves will be above background
+    transform: [
+      { scale: 0.3 },
+      { rotate: '5deg'}
+    ],
+  }
+});
