a2b6db50c5eecc542680f4973b7aa7d4d786b441
Scott Nguyen <scottowandmore@gmail.com>
2025-03-06 12:19:57 -0800

    5 6 7 8 10 + BackDrop.js

---
+++ components/SpeechBubble.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const SpeechBubble = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    id="Capa_1"
+    width="800"
+    height="800"
+    fill="#000"
+    version="1.1"
+    viewBox="0 0 71.015 71.015"
+  >
+    <path d="m13.223 67.768.022-15.913L0 51.729V3.247h71.015v48.5H36.446zM4 47.766l13.25.125-.016 12.251 17.967-12.395h31.814v-40.5H4z"></path>
+  </svg>
+);
+
+export default SpeechBubble;
